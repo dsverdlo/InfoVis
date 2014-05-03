bd4b893dfacc81685a2b5a6eed8f5855b5f15268
mathsaey <mathsaey@vub.ac.be>
2014-05-03 04:11:12 +0200

    Finished lastfm requests

--- scripts/backEnd/lastfm.js
+++ scripts/backEnd/lastfm.js
@@ -24,22 +24,28 @@ backEnd.lastFm.URL_API_KEY = "&api_key=46d561a6de9e5daa380db343d40ffbab";
 // Namespace Data //
 // -------------- //
 
-var ns = backEnd.lastFm;
-ns.cache = new backEnd.Cache();
-ns.URL_PRE = ns.URL_BASE + ns.URL_METHOD;
-ns.URL_POST = ns.URL_FORMAT_JSON + ns.URL_API_KEY;
+backEnd.lastFm.cache    = new backEnd.Cache();
+backEnd.lastFm.URL_PRE  = backEnd.lastFm.URL_BASE        + backEnd.lastFm.URL_METHOD;
+backEnd.lastFm.URL_POST = backEnd.lastFm.URL_FORMAT_JSON + backEnd.lastFm.URL_API_KEY;
 
 // ----------- //
 // Convenience //
 // ----------- //
 
-/* 
- * createUrl - Creates the url for the GET request.
- * @param method - The method of the request (e.g., artist.artistTopTracks)
- * @param optionList - A list of parameter-value tuples
- * @return url - The composited url for the request
+/** 
+ * Creates a URL for the last.fm API.
+ *
+ * \param method
+ *		The method of the request (e.g., artist.artistTopTracks)
+ * \param optionList
+ *		A list of parameter-value tuples,
+ *		general arguments such as the api key
+ *		are added by default.
+ * \return
+ *		The composed url for the request
  */
-ns.createUrl = function(method, optionList) {
+backEnd.lastFm.createUrl = function(method, optionList) {
+	var ns = backEnd.lastFm;
 	var url = ns.URL_PRE + method + ns.URL_POST;
 	
 	for(var i = 0; i < optionList.length; i++) {
@@ -49,12 +55,18 @@ ns.createUrl = function(method, optionList) {
 	return url;
 }
 
-/*
- * getHttp - Sends the HTTP GET request.
- * @param url - Url for the request
- * @return response - The http response (null if failed)
+/**
+ * Create a HTTP GET request, send it and return
+ * the reply it contains. 
+ * This doest __not__ happen asynchronously
+ *
+ * \param url
+ *		The url to send the request to.
+ * \return
+ *		The http response (null if failed)
  */
-ns.getHttp = function(url) {
+backEnd.lastFm.getHttp = function(url) {
+	var ns = backEnd.lastFm;
 	var cacheRes = ns.cache.get(url);
 	if (cacheRes) return cacheRes;
 
@@ -72,21 +84,28 @@ ns.getHttp = function(url) {
 }
 
 /*
- * containsError - Checks if a Last.FM object contains an error
- * @param object - Object to be checked
- * @return bool - Boolean indicating to containing error or not
+ * Checks if a Last.FM object contains an error
+ * \param object
+ *		Object to be checked
+ * \return 
+ *		True if the object contained an error.
+ *		False otherwise.
  */
-ns.containsError = function(object) {
+backEnd.lastFm.containsError = function(object) {
 	return (typeof(object) === "undefined") ||
 	       (typeof(object.error) === "number");
 }	
 
 /*
- * handleError - If there was an error from Last.FM, this function extracts it
- * @param object - The error object
- * @return list of error number and message
+ * Report an error to the user.
+ *
+ * \param object
+ *		The error object
+ * \return
+ *		An array containing the error number
+ *		and message.
  */
-ns.handleError = function(object) {
+backEnd.lastFm.handleError = function(object) {
 	var errorno = object.error;
 	var errormsg = object.message;
 	
@@ -94,200 +113,120 @@ ns.handleError = function(object) {
 	return [errorno, errormsg];
 }
 
+/**
+ * Create a request, send it, check
+ * if it returned any errors, and
+ * return the data it returned.
+ *
+ * \param method
+ *		The method to provide to createUrl
+ * \param optionList
+ *		The optionList to send to createUrl
+ * \return
+ *		A last.fm data object, or null
+ *		if something went wrong.
+ */
+backEnd.lastFm.request = function(method, optionList) {
+	var ns = backEnd.lastFm;
+	var url = ns.createUrl(method, optionList);
+	var obj = JSON.parse(ns.getHttp(url));
+
+	if(ns.containsError(obj)) {
+		ns.handleError(obj);
+		return null;
+	} else {
+		return obj;
+	}
+}
+
 // -------- //
 // Requests //
 // -------- //
 
-/*
- * getMetrosFor - Sends a GET request to retrieve list of available metros
- * @param location - Country for which to search on
- * @return metros - List of metros
- *
+/**
+ * Get the metros of a country
+ * \param country
+ *		The name of the country for which we want to receive the metros.
+ * \return
+ *		The last FM response to this request, in object form.
  */
-function getMetrosFor(location) {
-	var url = ns.createUrl("geo.getMetros", [["country", location]]);
-	var obj = JSON.parse(ns.getHttp(url));
-	
-	if(ns.containsError(obj)) {
-		return ns.handleError(obj)
-	}
-	
-	 // Uncomment for debug
-	// if(obj.metros.total == 0) {
-	// 	return document.writeln("<br>No results found");
-	// }
-	// metros = obj.metros.metro;
-	// for(var i = 0; i < metros.length; i++) {
-	// 	var metro = metros[i];
-	// 	document.writeln("<br/>" + metro.name);
-	// } 
-	
-	return obj.metros.metro;
-}
-
-/*
- * getArtistTopTracks - Gets the most popular tracks for a given artist
- * @param artist - Name of the artist
- * @return - List of tracks
- */
-function getArtistTopTracks(artist)
-{
-	var url = ns.createUrl("artist.getTopTracks", [["artist",artist]]);
-	
-	var obj = JSON.parse(ns.getHttp(url));
-	
-	if(ns.containsError(obj)) {
-		return ns.handleError(obj);
-	}
-	
-	/*
-	if(obj.toptracks.total == 0) {
-		return document.writeln("<br>No results found");
-	}
-	
-	tracks = obj.toptracks.track;
-	for(var i = 0; i < tracks.length; i++) {
-		var track = tracks[i];
-		document.writeln("<br\>" + (i+1) + ": " + track.name + " - " + track.duration);
-	} */
-	
-	return obj.toptracks;
-}
-
-/*
- * getMetroHypeTracks - Get hyped tracks for a given metro
- * @param country - The country of the metro
- * @param metro - The name of the metro
- * @return - List of hyped tracks
- */
-function getMetroHypeTracks(country, metro) 
-{
-	var url = ns.createUrl("geo.getMetroHypeTrackChart", [["country",country], ["metro", metro]]);
-	
-	var obj = JSON.parse(ns.getHttp(url));
-	
-	if(ns.containsError(obj)) {
-		return ns.handleError(obj);
-	}
-	
-	/*
-	if(obj.toptracks.total == 0) {
-		return document.writeln("<br>No results found");
-	}
-	tracks = obj.toptracks.track;
-	for(var i = 0; i < tracks.length; i++) {
-		var track = tracks[i];
-		document.writeln("<br\>" + (i+1) + ": " + track.name + " - " + track.artist.name);
-	} */
-	
-	return obj.toptracks;
-}
-
-/*
- * getTimeTrackChart - Gets the top track chart for a given metro and time
- * @param from - Unix seconds from when the data starts
- * @param to - Unix seconds until which period
- * @param country - Country of the metro
- * @param metro - Metro for which the chart needs to be looked up
- * @return list of top tracks
- */
-function getTimeTrackChart(from, to, country, metro) 
-{
-	var url = ns.createUrl("geo.getMetroHypeTrackChart", [["country",country], ["metro", metro], ["start", from],["end", to]]);
-	
-	var obj = JSON.parse(ns.getHttp(url));
-	
-	if(ns.containsError(obj)) {
-		return ns.handleError(obj);
-	}
-	
-	/*
-	if(obj.toptracks.total == 0) {
-		return document.writeln("<br>No results found");
-	}
-	tracks = obj.toptracks.track;
-	for(var i = 0; i < tracks.length; i++) {
-		var track = tracks[i];
-		document.writeln("<br\>" + (i+1) + ": " + track.name + " - " + track.artist.name);
-	} */
-	
-	return obj.toptracks;
+backEnd.lastFm.getCountryMetros = function(country) {
+	var res = backEnd.lastFm.request("geo.getMetros", [["country", country]]);
+	return res.metros.metro;
 }
 
 /**
  * Get the top artists for a country.
  *
  * \param country 
- *		The country for which you want to fetch results
+ *		The country for which we request the data
  * \param limit
  *		The amount of artists you want to fetch.
  * \return
- *		An array with Last FM artist objects
+ *		The last FM response to this request, in object form.
  */
-function getCountryTopArtists(country, limit) {
-	var url = ns.createUrl("geo.getTopArtists", [["country", country],["limit", limit]]);
-	var obj = JSON.parse(ns.getHttp(url));
+backEnd.lastFm.getCountryTopArtists = function(country, limit) {
+	var res = backEnd.lastFm.request(
+		"geo.getTopArtists", 
+		[["country", country],["limit", limit]]
+	);
+	return res.topartists.artist;
+}
 
-	if(ns.containsError(obj)) return ns.handleError(obj);
-	return obj.topartists.artist
+/**
+ * Get the top artists for a metro.
+ *
+ * \param country
+ *		The country of the metro.
+ * \param metro
+ *		The name of the metro.
+ * \param limit
+ *		The amount of artists you want to fetch.
+ * \return
+ *		The last FM response to this request, in object form.
+ */
+backEnd.lastFm.getMetroTopArtists = function(country, metro, limit) {
+	var res = backEnd.lastFm.request(
+		"geo.getMetroArtistChart", 
+		[["country", country], ["metro", metro]]
+	);
+	return res.topartists.artist;
 }
 
 /**
  * Get the top tracks for a country.
  *
  * \param country 
- *		The country for which you want to fetch results
+ *		The country for which we request the data
  * \param limit
  *		The amount of tracks you want to fetch.
  * \return
- *		An array with Last FM track objects
+ *		The last FM response to this request, in object form.
  */
-function getCountryTopTracks(country, limit) {
-	var url = ns.createUrl("geo.getTopTracks", [["country", country],["limit", limit]]);
-	var obj = JSON.parse(ns.getHttp(url));
-
-	if(ns.containsError(obj)) return ns.handleError(obj);
-	return obj.toptracks.track
+backEnd.lastFm.getCountryTopTracks = function(country, limit) {
+	var res = backEnd.lastFm.request(
+		"geo.getTopTracks", 
+		[["country", country],["limit", limit]]
+	);
+	return res.toptracks.track;
 }
 
 /**
- * Fetches the top track list for 
- * every country that we have data about.
+ * Get the top tracks for a metro.
+ *
+ * \param country
+ *		The country of the metro.
+ * \param metro
+ *		The name of the metro.
+ * \param limit
+ *		The amount of tracks you want to fetch.
+ * \return
+ *		The last FM response to this request, in object form.
  */
-function getGlobalTopTracks() {
-	var cacheRes = ns.cache.get("globTopTracks");
-	if (cacheRes) return cacheRes;
-
-	var result = [];
-	var countries = backEnd.countryList;
-
-	for (var i = 0; i < countries.length; i++) {
-		var country = countries[i];
-		var res = getCountryTopTracks(country.name, 1);
-		result.push({'country' : country, 'track': res});
-	};
-
-	ns.cache.put("globTopTracks", result)
-	return result;
-}
-
-/**
- * Fetches the top track list for 
- * every country that we have data about.
- */
-function getGlobalTopArtists() {
-	var cacheRes = ns.cache.get("globTopArtists");
-	if (cacheRes) return cacheRes;
-
-	var result = [];
-	var countries = backEnd.countryList;
-
-	for (var i = 0; i < countries.length; i++) {
-		var country = countries[i];
-		var res = getCountryTopArtists(country.name, 1);
-		result.push({'country' : country, 'artist': res});
-	};
-
-	ns.cache.put("globTopArtists", result)
-	return result;
+backEnd.lastFm.getMetroTopTracks = function(country, metro, limit) {
+	var res = backEnd.lastFm.request(
+		"geo.getMetroTrackChart", 
+		[["country", country], ["metro", metro]]
+	);
+	return res.toptracks.track;
 }
