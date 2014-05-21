ff629532f4b386ec3caca39b64cde391c28be84b
Kenny <kenny.deschuyteneer@gmail.com>
2014-05-21 20:39:18 +0200

    Made barchart functional

--- scripts/gui/barchart.js
+++ scripts/gui/barchart.js
@@ -7,8 +7,6 @@ var barchart = barchart || {};
 
 // This script bootstraps the barchart widget.
 
-barchart.tsv_data = "data/barchart.tsv"
-
 barchart.container = document.getElementById("container-barchart");
 
 barchart.total_width = parseInt(window.getComputedStyle(barchart.container).width, 10);
@@ -23,71 +21,77 @@ barchart.margin = {top: 20, right: 20, bottom: 20, left: 20};
 barchart.width = barchart.total_width - barchart.margin.left - barchart.margin.right;
 barchart.height = barchart.total_height - barchart.margin.top - barchart.margin.bottom;
 
+barchart.maximum_bars = 5;
+
+barchart.x_axis_offset = 20;
+barchart.bar_width = 20;
+
+barchart.y_axis_text = "World Track Chart";
+
 
 //// UI SETUP ///////////////////////////////////////////////////////////////////
 
 barchart.container = d3.select("#container-barchart");
 
-barchart.parseDate = d3.time.format("%d-%b-%y").parse;
-
-// The x scales with time, the y just scales linearly with track position.
-barchart.x = d3.time.scale().range([0, barchart.width]);
-barchart.y = d3.scale.linear().range([0, barchart.height]);
+// The x scalar maps the scales linearly over the top X (top 5, top 20, ...). The y scales
+// linearly over the popularity.
+barchart.x = d3.scale.linear().range([barchart.x_axis_offset, barchart.width]).domain([1, barchart.maximum_bars]);
+barchart.y = d3.scale.linear().range([0, barchart.height]).domain([100, 0]);
 
 // Initialize axes with our previously creates scalars.
-barchart.x_axis = d3.svg.axis().scale(barchart.x).orient("bottom");
-barchart.y_axis = d3.svg.axis().scale(barchart.y).ticks(5).orient("left");
-
-barchart.line = d3.svg.line()
-    .x(function(d) { return barchart.x(d.date); })
-    .y(function(d) { return barchart.y(d.position); });
+barchart.x_axis = d3.svg.axis().scale(barchart.x).tickValues([1, 2, 3, 4, 5]).orient("bottom");
+barchart.y_axis = d3.svg.axis().scale(barchart.y).orient("left");
 
 barchart.svg = barchart.container.append("svg")
-    .attr("width", barchart.width + barchart.margin.left + barchart.margin.right + barchart.offset)
+    .attr("width", barchart.width + barchart.margin.left + barchart.margin.right)
     .attr("height", barchart.width + barchart.margin.top + barchart.margin.bottom);
 
 barchart.group = barchart.svg.append("g")
     .attr("transform", "translate(" + barchart.margin.left + "," + barchart.margin.top + ")");
 
-d3.tsv(barchart.tsv_data, convert, function(error, data) {
-    barchart.x.domain(d3.extent(data, function(d) { return d.date; }));
-    barchart.y.domain([20, 1]);
+// Draw the x axis.
+barchart.group.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(" + barchart.offset / 2 + "," + barchart.height + ")")
+        .call(barchart.x_axis)
 
-    barchart.group.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(" + barchart.offset / 2 + "," + barchart.height + ")")
-            .call(barchart.x_axis)
-        .selectAll("text")
-            .attr("y", 0)
-            .attr("x", 9)
-            .attr("dy", ".35em")
-            .attr("transform", "rotate(90)")
-            .style("text-anchor", "start");
+// Draw the y axis.
+barchart.group.append("g")
+        .attr("class", "y axis")
+        .call(barchart.y_axis)
+    .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(barchart.y_axis_text);
 
-    barchart.group.append("g")
-            .attr("class", "y axis")
-            .call(barchart.y_axis)
-        .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Top tracks");
+gui.loadTracks = function() {
+    var tracks = backEnd.world.trackChart.slice(0, barchart.maximum_bars);
 
+    barchart.x_axis.tickFormat(function(d) { return d.name; })
+
+    // Draw the bars.
     barchart.group.selectAll(".bar")
-            .data(data)
+            .data(tracks)
         .enter().append("rect")
             .attr("class", "bar")
             .attr("rx", "5")
             .attr("ry", "5")
-            .attr("x", function(d) { return barchart.x(d.date) + (barchart.offset / 4); })
+            .attr("x", function(d) { return barchart.x(d.chartPos) })
             .attr("width", barchart.offset)
-            .attr("y", function(d) { return barchart.y(d.position); })
-            .attr("height", function(d) { return barchart.height - barchart.y(d.position); });
-});
-
-function convert(d) {
-    d.date = barchart.parseDate(d.date);
-    d.position = +d.position;
-    return d;
+            .attr("y", function(d) { return barchart.y(d.popularity * 100); })
+            .attr("height", function(d) { return barchart.height - barchart.y(d.popularity * 100); });
 };
+
+// Periodically check whether the backend has loaded yet.
+barchart.loading = function() {
+    if (backEnd.world.tracksReady()) {
+        console.log("Top tracks loaded!");
+        gui.loadTracks();
+    } else {
+        setTimeout(barchart.loading, 500);
+    };
+};
+
+barchart.loading();
