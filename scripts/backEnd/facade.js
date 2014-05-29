9f2db0cb3553e073ed69a84a54494280f75ee8db
mathsaey <mathsaey@vub.ac.be>
2014-05-29 16:41:35 +0200

    Added some clean abstraction for find top country

--- scripts/backEnd/facade.js
+++ scripts/backEnd/facade.js
@@ -53,11 +53,11 @@ backEnd.fetchAllData = function() {
 
 backEnd.fetchAllData();
 
-backEnd.findTopCountriesForTrack = function(name) {
+backEnd.findTopCountriesForChart = function(name, chartGetter) {	
 	var itemArr = [];
 	for (var i = 0; i < backEnd.countryList.length; i++) {
 		var country = backEnd.countryList[i];
-		var res = backEnd.findName(country.trackChart, name);
+		var res = backEnd.findName(country[chartGetter], name);
 		if (res) itemArr.push({item : res, country: country});
 	};
 
@@ -68,17 +68,10 @@ backEnd.findTopCountriesForTrack = function(name) {
 	return itemArr.slice(0,5);
 };
 
-backEnd.findTopCountriesForArtist = function(name) {
-	var itemArr = [];
-	for (var i = 0; i < backEnd.countryList.length; i++) {
-		var country = backEnd.countryList[i];
-		var res = backEnd.findName(country.artistChart, name);
-		if (res) itemArr.push({item : res, country: country});
-	};
+backEnd.findTopCountriesForTrack = function(name) {
+	return backEnd.findTopCountriesForChart(name, "trackChart");
+};
 
-	var sortFunc = function(l,r) {
-		return r.item.popularity - l.item.popularity;
-	}
-	itemArr.sort(sortFunc);
-	return itemArr.slice(0,5);
+backEnd.findTopCountriesForArtist = function(name) {
+	return backEnd.findTopCountriesForChart(name, "artistChart");
 };
